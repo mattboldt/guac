07c39c373c0f781b36c7e2f01d20ca176e93e646
Matt Boldt <me@mattboldt.com>
2018-08-08 23:23:09 -0500

    cleaned up response output

--- lib/git_pa/commands/up.rb
+++ lib/git_pa/commands/up.rb
@@ -17,37 +17,20 @@ module GitPa
       end
 
       def execute(input: $stdin, output: $stdout)
-        progress = begin_progress
         branch = ARGV[1] || 'master'
 
-        response = []
         @repos.each do |repo|
-          local_res = []
           git = Git.new(repo, branch)
-          response << "Updating #{repo.name.bold} on branch #{git.branch.underline}".colorize(:blue)
+          output.puts "Updating #{repo.name.bold} on branch #{git.branch.underline}".colorize(:blue)
 
-          local_res << git.checkout
-          local_res << git.pull
-          color, local_res = Colors.paint(local_res)
-          response << local_res
+          response = []
+          response << git.checkout
+          response << git.pull
+          color, response = Colors.paint(response)
 
+          output.puts response.join("\n")
           break if color == :red
-          progress.advance
         end
-
-        output.puts "\n"
-        output.puts response.flatten.join("\n")
-      end
-
-      private
-
-      def begin_progress
-        repo_names = @repos.map { |r| r.name.bold }.join(', ')
-        puts "Updating repos: #{repo_names}".colorize(:blue)
-
-        progress = TTY::ProgressBar.new('[:bar] :percent', total: @repos.length, width: 30)
-        progress.advance(0)
-        progress
       end
     end
   end
